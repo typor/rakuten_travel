5517eeda1c8ae14593ce34d27635debc41f88b11
kengos <kengo@kengos.jp>
2014-04-01 19:06:19 +0900

    Fix failing test

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -4,7 +4,7 @@ describe Hotel do
   describe '#save' do
     it '保存できること' do
       expect {
-        described_class.create(area: create(:area), no: '123', name: 'foo', postal_code: '123-4567', address1: 'foo', address2: 'bar', telephone_no: '03-1111-2222', access: 'baz', image_url: 'http://foo.com/', url: 'http://bar.com/')
+        described_class.create(area: create(:area), no: '123', long_name: 'foo', postal_code: '123-4567', address1: 'foo', address2: 'bar', telephone_no: '03-1111-2222', access: 'baz', image_url: 'http://foo.com/', url: 'http://bar.com/')
       }.to change{Hotel.count}.by(1)
     end
   end
