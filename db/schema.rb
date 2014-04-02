f997a2b6ca3117246e6b7f93fbb618febd770b9f
s-yamanaka <s_yamanaka@e-savaway.jp>
2014-04-02 21:20:17 +0900

    hotels add column

--- db/schema.rb
+++ db/schema.rb
@@ -28,20 +28,21 @@ ActiveRecord::Schema.define(version: 20140330143013) do
   add_index "areas", ["middle", "small", "detail"], name: "index_areas_on_middle_and_small_and_detail", unique: true
 
   create_table "hotels", force: true do |t|
-    t.integer  "area_id",                  null: false
-    t.string   "no",                       null: false
-    t.string   "long_name",                null: false
+    t.integer  "area_id",                      null: false
+    t.string   "no",                           null: false
+    t.string   "long_name",                    null: false
     t.string   "short_name"
-    t.string   "postal_code",              null: false
-    t.string   "address1",                 null: false
-    t.string   "address2",                 null: false
-    t.string   "telephone_no",             null: false
+    t.string   "postal_code",                  null: false
+    t.string   "address1",                     null: false
+    t.string   "address2",                     null: false
+    t.string   "telephone_no",                 null: false
     t.text     "image_url"
     t.text     "url"
     t.text     "access"
     t.string   "latitude"
     t.string   "longitude"
     t.integer  "room_num",     default: 0
+    t.boolean  "enabled",      default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
