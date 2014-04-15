fedb4cf8bde6a147c34fd04713c64f6c62c82011
kengos <kengo@kengos.jp>
2014-04-15 23:52:08 +0900

    add bootstrap-datepicker

--- app/controllers/admin/charges_controller.rb
+++ app/controllers/admin/charges_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ChargesController < Admin::ApplicationController
   def index
-    @search = Charge.search(params[:q])
+    @search = Charge.includes(:hotel, :plan, :room).search(params[:q])
     @search.sorts = 'stay_day asc' if @search.sorts.empty?
     @charges = @search.result.page params[:page]
   end
