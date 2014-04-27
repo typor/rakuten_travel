16e38be0025afac5a9d0a54fa890dc8e08b2ef88
kengos <kengo@kengos.jp>
2014-04-27 21:33:07 +0900

    using displayfeatures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -129,6 +129,7 @@ module ApplicationHelper
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', '#{code}', 'auto');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
 </script>
     EOS
